a9c6da67baceb9a8f4e199a90365786ca76346ec
Nicole <nicolerosasvallejo@hotmail.com>
2025-02-28 12:28:58 -0600

    Prueba CORS

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,13 +2,15 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
-        const whiteList = [process.env.URL_FRONTEND, undefined];
+        console.log("Solicito", origin);
+        const whiteList = [process.env.URL_FRONTEND];
 
         if(process.argv.includes('--api')) {
             whiteList.push(undefined);
         }
 
         if(whiteList.includes(origin)) {
+            console.log("Permitido");
             callback(null, true);
         }
         else {
